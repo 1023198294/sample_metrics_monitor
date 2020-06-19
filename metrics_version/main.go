42fef77e18f4d438c2332141263fe26cb61456e9
Huang Yangjun <1023198294@qq.com>
2020-06-19 13:39:25 +0800

    Update main.go

--- metrics_version/main.go
+++ metrics_version/main.go
@@ -7,18 +7,30 @@ import (
 	"os"
 	"strconv"
 	"example/metrics"
+	"github.com/shirou/gopsutil/mem" //用于监控CPU和内存的包
 )
 
 func main(){
 	http.HandleFunc("/abc", index)
+	http.HandleFunc("/usage", usage)
 	http.Handle("/metrics", promhttp.Handler())
+
 	metrics.Register()
 	err := http.ListenAndServe(":5565", nil) // 设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
-
+func usage(w http.ResponseWriter,r *http.Request){
+	timer:=metrics.NewAdmissionLatency()
+	metrics.RequestIncrease()
+	v,_ := mem.VirtualMemory() //获取CPU占用率情况
+	_,err := w.Write([]byte("Total memory: %v, UsedPercent:%f%%\n",v.Total,v.Free,v.UsedPercent))
+	if err!=nil{
+		log.Println("err:"+err.Error()+" Yes\n")
+	}
+	timer.Observe()
+}
 func index(w http.ResponseWriter, r *http.Request) {
 	timer:=metrics.NewAdmissionLatency()
 	metrics.RequestIncrease()
